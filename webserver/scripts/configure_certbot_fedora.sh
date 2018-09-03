66742c94bfd3367bdc35b3411de05a39dafb3c4d
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2018-09-03 10:09:17 -0300

    Melhorias

--- webserver/scripts/configure_certbot_fedora.sh
+++ webserver/scripts/configure_certbot_fedora.sh
@@ -7,10 +7,10 @@ dnf install python3*certbot* -y
 
 # passo 2: Gerar os certificados no sistema para que na próxima vez, a renovação seja possível
 DOMAIN=meudominio.com
-certbot certonly --manual -d *.$DOMAIN -d $DOMAIN --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
+certbot certonly --manual -d $DOMAIN -d *.$DOMAIN --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
 
 # passo 3: Adicionar no final do crontab para renovar periodicamente
-echo "39      1,13    *       *       *       root    certbot renew --no-self-upgrade" >> /var/spool/cron/root
+echo "39 1,13 * * * root certbot renew --no-self-upgrade" >> /var/spool/cron/root
 
 # passo 4: Configurar seu vhost da seguinte forma
 #SSLEngine on
@@ -18,4 +18,3 @@ echo "39      1,13    *       *       *       root    certbot renew --no-self-up
 #SSLCipherSuite ALL:!DH:!EXPORT:!RC4:+HIGH:+MEDIUM:!LOW:!aNULL:!eNULL
 #SSLCertificateFile /etc/letsencrypt/live/meudominio.com/cert.pem
 #SSLCertificateKeyFile /etc/letsencrypt/live/meudominio.com/privkey.pem
-
