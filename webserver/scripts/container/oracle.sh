66d5f72c1377e0897c5939d90ded2df95d6f824f
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2018-09-22 11:35:25 -0300

    .

--- webserver/scripts/container/oracle.sh
+++
@@ -1,2 +0,0 @@
-docker rm oracle
-docker run --name oracle --network=docker-webserver_default -d -p 8080:8080 -p 1521:1521 -v /my/oracle/data:/u01/app/oracle -e DBCA_TOTAL_MEMORY=1024 sath89/oracle-12c
--- webserver/scripts/container/sqlserver.sh
+++
@@ -1,2 +0,0 @@
-docker rm sqlserver
-docker run --name sqlserver --network=docker-webserver_default -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=yourStrong(!)Password" -e "MSSQL_PID=Express" -p 1433:1433 -d microsoft/mssql-server-linux:latest
