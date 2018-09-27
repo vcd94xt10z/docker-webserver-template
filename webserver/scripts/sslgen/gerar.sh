05cc3b1c6c4e210d81c6d436363111bfe7e499fd
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2018-09-26 21:01:21 -0300

    .

--- webserver/scripts/sslgen/gerar.sh
+++ webserver/scripts/sslgen/gerar.sh
@@ -5,6 +5,8 @@ FOLDER_OUT=${FOLDER}out/
 DAYS=99999
 
 mkdir ${FOLDER_OUT}
+cd ${FOLDER_OUT}
+
 openssl genrsa -out ${FOLDER_OUT}server_rootCA.key 2048
 openssl req -x509 -new -nodes -key ${FOLDER_OUT}server_rootCA.key -sha256 -days ${DAYS} -out ${FOLDER_OUT}server_rootCA.pem
 openssl req -new -sha256 -nodes -out ${FOLDER_OUT}server.csr -newkey rsa:2048 -keyout ${FOLDER_OUT}server.key -config <( cat ${FOLDER}server_rootCA.csr.cnf )
